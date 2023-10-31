5cab8060f617b3653d00a5a68705c5d9dae7e8f5
MichaelDepp <michaeldepp7@gmail.com>
2023-11-01 01:16:48 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const GOOGLE_PLACES_API_KEY = 'YOUR API KEY';
+
+const App = () => {
+	const [selectedLocation, setSelectedLocation] = useState({
+		latitude: 3.1470984, // Maybank Location
+		longitude: 101.6990835, // Maybank Location
+	});
+	const mapRef = useRef(null);
+
+	const { height, width } = Dimensions.get('window');
+	const LATITUDE_DELTA = 0.009;
+	const LONGITUDE_DELTA = LATITUDE_DELTA * (width / height);
+
+	const marker = {
+		latlng: selectedLocation,
+	};
+
+	const updateLocation = (data, details) => {
+		const { lat, lng } = details.geometry.location;
+		const newLocation = {
+			latitude: lat,
+			longitude: lng,
+		};
+		setSelectedLocation(newLocation);
+		mapRef.current.animateToRegion({
+			...newLocation,
+			latitudeDelta: 0.0922,
+			longitudeDelta: 0.0421,
+		});
+	};
+
+	return (
+		<View style={styles.container}>
+			<MapView
+				ref={mapRef}
+				style={styles.container}
+				initialRegion={{
+					latitude: selectedLocation.latitude,
+					longitude: selectedLocation.longitude,
+					latitudeDelta: LATITUDE_DELTA,
+					longitudeDelta: LONGITUDE_DELTA,
+				}}
+			>
+				<Marker coordinate={marker.latlng} />
+			</MapView>
+			<View style={styles.googlePlaceContainer}>
+				<GooglePlacesAutocomplete
+					styles={{
+						textInputContainer: textInputContainer,
+					}}
+					placeholder="Search"
+					query={{
+						key: GOOGLE_PLACES_API_KEY,
+						language: 'en',
+					}}
+					fetchDetails
+					onPress={updateLocation}
+					onFail={(error) => console.error(error)}
+				/>
+			</View>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#ecf0f1',
+	},
+	textInputContainer: {
+		backgroundColor: 'rgba(0,0,0,0)',
+		borderTopWidth: 0,
+		borderBottomWidth: 0,
+		width: '100%',
+	},
+	googlePlaceContainer: {
+		position: 'absolute',
+		top: 50,
+		left: 0,
+		right: 0,
+		width: '100%',
+		alignSelf: 'center',
+		paddingHorizontal: 30,
+	},
 });
+
+export default App;
