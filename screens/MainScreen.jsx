126a7a05fb8669611ef223fc0bccf6312cf42c3d
MichaelDepp <michaeldepp7@gmail.com>
2023-11-02 00:11:37 +0800

    feat : use the dispatch and selector elements from redux to consume the state from redux

--- screens/MainScreen.jsx
+++ screens/MainScreen.jsx
@@ -3,12 +3,18 @@ import { View, StyleSheet } from "react-native";
 import MapContainer from "../components/MapContainer";
 import Autocomplete from "../components/Autocomplete";
 
+import { useDispatch, useSelector } from "react-redux"
+import { addPlace } from "../context/actions/placesActions"
+
 const HomeScreen = () => {
   const [selectedLocation, setSelectedLocation] = useState({
     latitude: 3.1470984, // Location
     longitude: 101.6990835, // Location
   });
   const mapRef = useRef(null);
+  const dispatch = useDispatch();
+
+  const searchedPlaces = useSelector((state) => state.places.searchedPlaces);
 
   const updateLocation = (data, details) => {
     const { lat, lng } = details.geometry.location;
@@ -22,12 +28,17 @@ const HomeScreen = () => {
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     });
+    const location = {
+      description: data.description,
+      geometry: details.geometry,
+    }
+    dispatch(addPlace(location));
   };
 
   return (
     <View style={styles.container}>
-      <MapContainer selectedLocation={selectedLocation} mapRef={mapRef} />
-      <Autocomplete onUpdateLocation={updateLocation} />
+      <MapContainer selectedLocation={selectedLocation} mapRef={mapRef}/>
+      <Autocomplete onUpdateLocation={updateLocation} searchedPlaces={searchedPlaces}/>
     </View>
   );
 };
@@ -39,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default HomeScreen;
+export default HomeScreen
