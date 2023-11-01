79bd888c97457714257f8f32ac8d3ce199b4e504
MichaelDepp <michaeldepp7@gmail.com>
2023-11-02 00:12:25 +0800

    refactor : wrap the application with the redux store provider

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import HomeScreen from './screens/MainScreen';
+import { Provider } from 'react-redux';
+import store from './context/store';
 
 const App = () => {
 	return (
-		<HomeScreen />
+		<Provider store={store}>
+			<HomeScreen />
+		</Provider>
 	);
 };
 
