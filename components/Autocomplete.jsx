66a29b82e4d7a62acd2594f50d179bffcae6affe
michaelsm <michaelsm@maxis.com.my>
2023-11-01 11:51:20 +0800

    feat : added the autocomplete textinput component

---
+++ components/Autocomplete.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+
+const GOOGLE_PLACES_API_KEY = "AIzaSyCd8xOrhNvkxauR0TSSOJG5VxaO2p5b2Bg";
+
+const Autocomplete = ({onUpdateLocation}) => {
+
+  return (
+    <View style={styles.googlePlaceContainer}>
+      <GooglePlacesAutocomplete
+        styles={{
+          textInputContainer: textInputContainer,
+        }}
+        placeholder="Search"
+        query={{
+          key: GOOGLE_PLACES_API_KEY,
+          language: "en",
+        }}
+        fetchDetails
+        onPress={onUpdateLocation}
+        onFail={(error) => console.error(error)}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textInputContainer: {
+    backgroundColor: "rgba(0,0,0,0)",
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    width: "100%",
+  },
+  googlePlaceContainer: {
+    position: "absolute",
+    top: 50,
+    left: 0,
+    right: 0,
+    width: "100%",
+    alignSelf: "center",
+    paddingHorizontal: 30,
+  },
+});
+
+export default Autocomplete;
